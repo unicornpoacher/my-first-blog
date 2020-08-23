e58ed2ae9effb8d6121662bf14a4f4adf43b21cc
Mark <test@test.com>
2020-08-23 15:58:05 +0800

    Added view and template for detailed blog post as well as CSS for the site

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post #from the Post class defined in models.py in the blog directory
 
-admin.site.register(Post)
+admin.site.register(Post)#to make post available on the admin page, need to register
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig', #added
 ]
 
 MIDDLEWARE = [
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #added
